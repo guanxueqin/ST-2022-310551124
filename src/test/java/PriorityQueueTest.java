3d951681f4346a8a4089031ec9ea8877b5480046
guanxueqin <101779082+guanxueqin@users.noreply.github.com>
2022-03-17 11:26:56 +0800

    Update PriorityQueueTest.java

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -26,22 +26,22 @@ public class PriorityQueueTest {
     }
 
     public static Stream<Arguments> streamProvider() {
-        return Stream.of(
-                Arguments.of(new int[] {2, 4, 3}, new int[] {2, 3, 4}),
-                Arguments.of(new int[] {8, 7, 8, 7, 8}, new int[] {7, 7, 8, 8, 8}),
-                Arguments.of(new int[] {-5, -4, -3, -2, -1}, new int[]{-5, -4, -3, -2, -1}),
-                Arguments.of(new int[] {7, 9, 5, 8, -1}, new int[] {-1, 5, 7, 8, 9}),
-                Arguments.of(new int[]{Integer.MAX_VALUE, 0, Integer.MIN_VALUE}, new int[]{Integer.MIN_VALUE, 0, Integer.MAX_VALUE})
-        );
+//         return Stream.of(
+//                 Arguments.of(new int[] {2, 4, 3}, new int[] {2, 3, 4}),
+//                 Arguments.of(new int[] {8, 7, 8, 7, 8}, new int[] {7, 7, 8, 8, 8}),
+//                 Arguments.of(new int[] {-5, -4, -3, -2, -1}, new int[]{-5, -4, -3, -2, -1}),
+//                 Arguments.of(new int[] {7, 9, 5, 8, -1}, new int[] {-1, 5, 7, 8, 9}),
+//                 Arguments.of(new int[]{Integer.MAX_VALUE, 0, Integer.MIN_VALUE}, new int[]{Integer.MIN_VALUE, 0, Integer.MAX_VALUE})
+//         );
 
         /* Failure version to test CI */
-        // return Stream.of(
-        //         Arguments.of(new int[] {2, 4, 3}, new int[] {3, 3, 4}),
-        //         Arguments.of(new int[] {4, 6, 2, 0}, new int[] {1, 2, 4, 6}),
-        //         Arguments.of(new int[] {-8, 6, 0, -4}, new int[] {-7, -4, 0, 6}),
-        //         Arguments.of(new int[] {7, 9, 5, 8, -1}, new int[] {0, 5, 7, 8, 9}),
-        //         Arguments.of(new int[] {3, 6, 9, -2}, new int[] {-1, 3, 6, 9})
-        // );
+        return Stream.of(
+                Arguments.of(new int[] {2, 4, 3}, new int[] {3, 3, 4}),
+                Arguments.of(new int[] {4, 6, 2, 0}, new int[] {1, 2, 4, 6}),
+                Arguments.of(new int[] {-8, 6, 0, -4}, new int[] {-7, -4, 0, 6}),
+                Arguments.of(new int[] {7, 9, 5, 8, -1}, new int[] {0, 5, 7, 8, 9}),
+                Arguments.of(new int[] {3, 6, 9, -2}, new int[] {-1, 3, 6, 9})
+        );
     }
 
     @Test
